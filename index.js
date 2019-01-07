382c8a56ec80f3929f6c517606c8b38df58fcf7c
Theta Wang <thetawang@ThetadeMac-mini.local>
2019-01-07 12:10:49 +0800

    test

--- index.js
+++ index.js
@@ -47,9 +47,9 @@ function replyMessage(messege){
     messege = "不是很好！";
     return messege
 
-  }else if(messege.search("選一個")){
+  }else if(messege.search("找一個")){
     console.log('----------------information----------------');
-    console.log('process: messege.search("選一個")');
+    console.log('process: messege.search("找一個")');
     console.log('-------------------------------------------');
     var optionArray=[];
     messege=messege.substring(messege.search("\n")+2);
